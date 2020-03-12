41e7bc19a80cabc6cc454f328ae68802526df284
oscargomez87 <oscarstiven87@gmail.com>
2020-03-12 13:47:40 -0500

    Function that adds a node to the right of the parent node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Adds a node to the right of a parent node
+ *
+ * @parent: parent of the new node
+ * @value: value of the node
+ * Return: address of the new node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	if (parent->right != NULL)
+	{
+		node->right = binary_tree_node(node, parent->right->n);
+		free(parent->right);
+	}
+	parent->right = node;
+	return (node);
+}
