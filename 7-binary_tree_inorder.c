fd60f509b536f84b8be974fe91a54afcff5f67d3
oscargomez87 <oscarstiven87@gmail.com>
2020-03-13 13:50:41 -0500

    Changed function logic removing unnecessary conditions.

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -8,14 +8,10 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
-		return;
-	if (tree->left != NULL)
+	if (tree != NULL && func != NULL)
+	{
 		binary_tree_inorder(tree->left, func);
-	if (tree->left == NULL && tree->right == NULL)
 		func(tree->n);
-	if (tree->left != NULL && tree->right != NULL)
-		func(tree->n);
-	if (tree->right != NULL)
 		binary_tree_inorder(tree->right, func);
+	}
 }
