199f17ebff95a97f8a49e4574e368a72e63c6b6b
oscargomez87 <oscarstiven87@gmail.com>
2020-03-13 13:52:31 -0500

    Changed function logic removing unnecessary conditions.

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,11 +9,10 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
-		return;
-	func(tree->n);
-	if (tree->left != NULL)
+	if (tree != NULL && func != NULL)
+	{
+		func(tree->n);
 		binary_tree_preorder(tree->left, func);
-	if (tree->right != NULL)
 		binary_tree_preorder(tree->right, func);
+	}
 }
